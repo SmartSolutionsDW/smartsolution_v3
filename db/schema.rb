b939f25a760e70a38b0b8b14f776da732685e5d7
manfredp88 <manfredp88@gmail.com>
2016-09-26 19:22:25 +0000

    Aplicando bootstrap a vista sala

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926054943) do
+ActiveRecord::Schema.define(version: 20160926191348) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "nombre",     limit: 255
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20160926054943) do
   end
 
   add_index "productos", ["tipo_producto_id"], name: "index_productos_on_tipo_producto_id", using: :btree
+
+  create_table "salas", force: :cascade do |t|
+    t.integer  "co_sala",      limit: 4
+    t.integer  "co_local",     limit: 4
+    t.integer  "no_sala",      limit: 4
+    t.integer  "qt_capacidad", limit: 4
+    t.text     "description",  limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "tipo_productos", force: :cascade do |t|
     t.string   "Co_TipoProducto",   limit: 255
