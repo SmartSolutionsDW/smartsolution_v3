bb6c44c325248d85b9c150b908c6fee71269c2cb
smauricio <emauricioh@gmail.com>
2016-10-06 01:58:46 +0000

    Validación tipo producto

--- app/models/tipo_producto.rb
+++ app/models/tipo_producto.rb
@@ -1,3 +1,9 @@
 class TipoProducto < ActiveRecord::Base
     has_many :producto
+    
+    validates :name, presence: true, 
+                    uniqueness: {case_sensitive: false}
+                    
+    validates :estado, presence: true, 
+                    uniqueness: {case_sensitive: false}
 end
